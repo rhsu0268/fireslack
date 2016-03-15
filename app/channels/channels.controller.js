e62add7a6b919a7be0bab4ac04ba342dcf5a676d
Richard Hsu <rhsu@gwu.edu>
2016-03-14 23:24:01 -0400

    Added direct messaging

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -7,6 +7,7 @@ angular.module('angularfireSlackApp')
         channelsCtrl.channels = channels;
         channelsCtrl.getDisplayName = Users.getDisplayName;
         channelsCtrl.getGravatar = Users.getGravatar;
+        channelsCtrl.users = Users.all;
 
         channelsCtrl.logout = function()
         {
